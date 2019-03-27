7e0139cececb52e5ea5f791ef1c6d67b3cd5171a
Fernanda Rodrigues da Silva <fernanda.rodrigues.cdc@gmail.com>
2019-03-27 01:48:05 -0700

    Fixing typo on client email config var

--- config/prod.js
+++ config/prod.js
@@ -2,8 +2,7 @@ module.exports = {
     googleProjectID: process.env.GOOGLE_PROJECT_ID,
     dialogFlowSessionID: process.env.DIALOGFLOW_SESSION_ID,
     dialogFlowSessionLanguageCode: process.env.DIALOGFLOW_LANGUAGE_CODE,
-    googleClienteEmail: process.env.GOOGLE_CLIENT_EMAIL,
+    googleClientEmail: process.env.GOOGLE_CLIENT_EMAIL,
     googlePrivateKey: process.env.GOOGLE_PRIVATE_KEY,
     mongoURI: process.env.MONGO_URI,
-    googleClientEmail: GOOGLE_CLIENT_EMAIL
 }
