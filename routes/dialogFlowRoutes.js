967066c2c52242c9ff540051e2ddab8abe15be44
Fernanda Rodrigues da Silva <fernanda.rodrigues.cdc@gmail.com>
2019-04-03 19:35:09 -0700

    Fixing heroku deploy

--- routes/dialogFlowRoutes.js
+++ routes/dialogFlowRoutes.js
@@ -2,10 +2,10 @@ const chatbot = require('../chatbot/chatbot');
 
 module.exports = app => {
 
-    app.get('/', (req, res) => {
-        console.log('dialogFlowRoutes "/" route');
-        res.send({'hello': 'Fernanda'});
-    });
+    // app.get('/', (req, res) => {
+    //     console.log('dialogFlowRoutes "/" route');
+    //     res.send({'hello': 'Fernanda'});
+    // });
     
     app.post('/api/df_text_query', async (req, res) =>{
         let responses = await chatbot.textQuery(req.body.text, req.body.parameters);
